63b1720ea7f9a3ead64af6f3b8e7985fca4283d4
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:03:50 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_alert_rule_ms_security_incidents" {
+  description = "All sentinel_alert_rule_ms_security_incident resources"
+  value       = azurerm_sentinel_alert_rule_ms_security_incident.sentinel_alert_rule_ms_security_incidents
+}
+
